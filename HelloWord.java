0bc4fc5b52d5c9c2a6b3175a711f4ef79b726ae1
bobo0915 <bobo0915@users.noreply.github.com>
2015-11-06 21:44:28 +0800

    Update HelloWord.java

--- HelloWord.java
+++ HelloWord.java
@@ -49,7 +49,7 @@ public class API01 {
 		/*String httpUrl = "http://apis.baidu.com/txapi/mvtp/meinv"; 美女图片
 		String httpArg = "num=10";*/
 		String httpUrl = "http://apis.baidu.com/apistore/idservice/id";
-		String httpArg = "id=420683199009153410";
+		String httpArg = "id=420683199609163410";
 		String jsonResult = request(httpUrl, httpArg);
 		Map map =  new HashMap();
 	    Object ojb= JSONObject.parse(jsonResult);
